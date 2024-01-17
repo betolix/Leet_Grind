966a3bafa3c9159c861d15cc622443730b16fd41
betolix <betolix@gmail.com>
2024-01-17 17:26:57 +0000

    Valid Palindrome Improveable Performance Beats 80.75% of users with C++

--- 0010_Valid_Palindrome.cpp
+++ 0010_Valid_Palindrome.cpp
@@ -7,15 +7,18 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if(s.length() < 1) return true;
-        int st = 0, en = s.length()-1;
-        while(st < en){
-            while(st < en && !isalnum(s[st])) st++;
-            while(st < en && !isalnum(s[en])) en--;
-            if(st < en && tolower(s[st]) != tolower(s[en])) return false;
-            st++; en--;
-        }
-        return true;
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
     }
 };
 
