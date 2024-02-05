6f58996e696c17f054793bd42951445b80d3be45
betolix <betolix@gmail.com>
2024-02-05 22:37:33 +0000

    Same Tree PEND

---
+++ 0017_Same_Tree_PEND.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+    }
+};
+
+int main() {
+    
+    Solution solution;
+    
+    
+}
+
+
+/*
+
+100. Same Tree
+Easy
+Topics
+Companies
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+ 
+
+Example 1:
+
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+Example 2:
+
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+Example 3:
+
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+Seen this question in a real interview before?
+1/4
+Yes
+No
+Accepted
+1.9M
+Submissions
+3.1M
+Acceptance Rate
+60.6%
+Topics
+Companies
+Discussion (96)git add 
+
+*/
